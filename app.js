a92d6841892df672bed01974098bf937b32e4bc8
David Fonseca <david.fonseca@eisla.local>
2022-04-07 13:03:14 -0500

    Changes in object to list

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 var cities = require('cities');
+const res = require('express/lib/response');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -14,7 +15,21 @@ app.post("/cities", function (req, res){
     
     
    // console.log(cities.zip_lookup(zipcode))
-    res.send(cities.zip_lookup(zipcode));
+
+   let _response = cities.zip_lookup(zipcode);
+
+    //res.send("Zip Code: " +  _response.zipcode
+  //  + " City: " + _response.city);
+    res.send(`<p>Zip Code:${_response.zipcode}</p>
+              <p>City: ${_response.city}</p>
+              <p>City: ${_response.city}</p>
+              <p>State: ${_response.state}</p>
+              <p>State Abbr:${_response.state_abbr}</p>
+              <p>Latitude:${_response.latitude}</p>
+              <p>Longitude:${_response.longitude}</p>`)
+   
+    
+   
 });
 
 app.get('/', (req, res) => {
